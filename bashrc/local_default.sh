ac5dcfa3ecba3815564c7e2bdfbddf53fc3739b0
Matthew Woitaszek <mattheww@ucar.edu>
2011-11-02 15:29:07 -0600

    Added default local profile

---
+++ bashrc/local_default.sh
@@ -0,0 +1,30 @@
+#
+# Matthew's profile for all other systems
+#
+# July 2011
+#
+
+# -------------------------------------
+# Prompt Configuration
+# -------------------------------------
+
+# Polynya is teal by default
+PROMPT_HOSTCOLOR="\[\033[0;36m\]"
+
+# For certain hosts, override the colors
+#case `hostname` in
+#
+#    # Back-end nodes: teal on dark grey
+#    dav* | dcs* )
+#        PROMPT_HOSTCOLOR="\[\033[0;36;100m\]"
+#        ;;
+#esac
+
+
+matthew_prompt
+
+
+# -------------------------------------
+# Local Configuration
+# -------------------------------------
+
