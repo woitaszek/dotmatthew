ae829eae804cb14998ea6d958a34fb815aa6653f
Matthew Woitaszek <matthew@occipital.com>
2011-12-07 08:43:34 -0700

    Added new configurations with new colors

---
+++ bashrc/local_icedecaf.sh
@@ -0,0 +1,59 @@
+#
+# Matthew's profile for 'icedecaf'
+# MacBook Pro / Snow Leopard
+# December 2011
+#
+
+#
+# This is 
+#   .bashrc.local -> ~/.matthew/profile/local_notyet.sh
+# It relies on variables and functions defined by 
+#   .bashrc.global -> ~/.matthew/profile/global.sh
+# which should be called before this script.
+#
+
+# -------------------------------------
+# Prompt Configuration
+# -------------------------------------
+PROMPT_HOSTCOLOR="\[\033[0;94m\]"
+matthew_prompt
+
+
+# -------------------------------------
+# Path Configuration
+# -------------------------------------
+
+# Path overrides for packages
+
+# Path overrides for source packages installed in /opt
+
+# -------------------------------------
+# Application Configuration
+# -------------------------------------
+
+# Unison
+export UNISONLOCALHOSTNAME=icedecaf
+
+# Mac: use ls with colors
+export CLICOLOR="True"
+
+# Vim on Mac: Don't rely on X11
+#alias vim="vim -X"
+
+# GNU privacy guard... should be set out-of-repository
+#export GNUPGHOME=""
+
+# -------------------------------------
+# Everything Else
+# -------------------------------------
+
+#
+# Occipital production staging
+#
+tothestage(){
+  git push stageserver stage; git push origin stage
+}
+totheprod(){
+  git push masterserver master; git push slaveserver master; git push origin master;
+}
+
---
+++ bashrc/local_oc.sh
@@ -0,0 +1,40 @@
+#
+# Matthew's profile for all other systems
+#
+# July 2011
+#
+
+# -------------------------------------
+# Prompt Configuration
+# -------------------------------------
+
+# Unconfigured servers are teal by default
+PROMPT_HOSTCOLOR="\[\033[0;36m\]"
+
+# For certain hosts, override the colors
+hostname_sha=`echo \`hostname\` | shasum | awk '{print $1}'`
+case `hostname_sha` in
+
+    # primary and secondary servers
+    4eb7* )
+        PROMPT_HOSTCOLOR="\[\033[0;91;41m\]" # bright red on dark red
+        ;;
+    8a47* )
+        PROMPT_HOSTCOLOR="\[\033[0;93;41m\]" # yellow on dark red
+        ;;
+
+    # development stage
+    b952* )
+        PROMPT_HOSTCOLOR="\[\033[0;30;103m\]" # on yellow
+        ;;
+
+    # back-end processing
+esac
+
+matthew_prompt
+
+
+# -------------------------------------
+# Local Configuration
+# -------------------------------------
+
