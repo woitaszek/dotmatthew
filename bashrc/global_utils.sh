4f81a70d239abe6695f51135acc4ec46bc9badaa
Matthew Woitaszek <matthew.woitaszek@gmail.com>
2015-01-20 21:33:22 -0800

    Search for git tools in more locations

--- bashrc/global_utils.sh
+++ bashrc/global_utils.sh
@@ -8,11 +8,26 @@
 # Git
 # -------------------------------------
 
+# Source new-style git prompt preparation
+if [ -e /usr/share/git-core/contrib/completion/git-prompt.sh ]
+then
+    source /usr/share/git-core/contrib/completion/git-prompt.sh
+elif [ -e /Library/Developer/CommandLineTools/usr/share/git-core/git-prompt.sh ]
+then
+    source /Library/Developer/CommandLineTools/usr/share/git-core/git-prompt.sh
+elif [ -e /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-prompt.sh ]
+then
+    source /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-prompt.sh
+fi
+
 # Source the git-completion library for use when constructing
 # the prompt.
-if [ -e /usr/share/git-core/git-completion.bash ]
+if [ -e /usr/share/doc/git-1.8.3.1/contrib/completion/git-completion.bash ]
 then
-    # First choice -- works on Macs
+    source /usr/share/doc/git-1.8.3.1/contrib/completion/git-completion.bash
+elif [ -e /usr/share/git-core/git-completion.bash ]
+then
+    # Works on macs
     source /usr/share/git-core/git-completion.bash
 elif [ -e /usr/local/git/contrib/completion/git-completion.bash ]
 then
