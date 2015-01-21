b5b6c485834157473f4983beb30ce7850c699759
Matthew Woitaszek <matthew.woitaszek@gmail.com>
2015-01-20 21:32:19 -0800

    Update prompt coloring components

--- bashrc/global_color.sh
+++ bashrc/global_color.sh
@@ -4,55 +4,71 @@
 # July 2011
 #
 
+# I used to use this. This is much easier to edit.
+#  export PS1="\[\033]0;\u@\h: \w\007\]\[\033[1;32m\]\@ 
+#    \[\033[0m\]\[\033[1;1m\]\[\033[1;31m\]\!\[\033[0m\] [\u@\h \W]\$ "
+
 function matthew_prompt
 {
     #
     # This function sets the prompt. Before calling this function,
-    # the local bash profile script should set the following local 
+    # the local bash profile script may set the following local 
     # environment variables to an ANSI color string.
-    #   PROMPT_HOSTCOLOR
+    #
+    #   PROMPT_HOSTCOLOR - color for hostname, uncolored by default
+    #   PROMPT_USERCOLOR - color for username, by default, red if not matthew
     #
 
-    # I used to use this. The new method is a great improvement!
-    #  export PS1="\[\033]0;\u@\h: \w\007\]\[\033[1;32m\]\@ 
-    #    \[\033[0m\]\[\033[1;1m\]\[\033[1;31m\]\!\[\033[0m\] [\u@\h \W]\$ "
     local NOCOLOR="\[\033[0m\]"
+
+
 
     #
     # Terminal information for title bars
     #
+
     if [[ $TERM == xterm* || $OSTYPE == darwin* || $TERM == screen ]]
     then
-        TITLESTR="\[\033]0;\u@\h:\w\007\]"
+        local TITLESTR="\[\033]0;\u@\h:\w\007\]"
     else
-        TITLESTR=""
+        local TITLESTR=""
     fi
 
-    # 
-    # Global checks
+
+
+    #
+    # Prompt component additions - used for warnings, git status, etc.
     #
 
-    # Set the prompt to red if the user account is not matthew or mattheww
-    local PROMPT_USERCOLOR=$NOCOLOR
+    # Username coloring for nonstandard user accounts
+    if [ -z "$PROMPT_USERCOLOR" ]
+    then
+        local PROMPT_USERCOLOR=$NOCOLOR
         [[ "$USER" != "mwoitas" && "$USER" != "matthew" ]] && PROMPT_USERCOLOR="\[\033[1;31m\]"
+    fi
 
     # Look at VIFs to determine if this system might be running Xen.
     vif_count=`/sbin/ifconfig 2>/dev/null | grep vif | awk -F. '{print $1}' | sort | uniq  | wc -l`
     if [[ "$vif_count" -gt "0" ]]
     then
-        WARNSTR=" [\[\033[4;41m\]XEN${NOCOLOR}]"
+        local WARNSTR=" [\[\033[4;41m\]XEN${NOCOLOR}]"
     else
-        WARNSTR=""
+        local WARNSTR=""
     fi
     
     # Determine if __git_ps1 is available
     type -t __git_ps1 > /dev/null
     if [[ "$?" -eq "0" ]]
     then
+        # Note that we are including a dynamic evaluation via \$;
+        # we are trusting that it is being sanitized for us.
+        # Prefix with a space if not empty
         local GITSTR="\$(__git_ps1 \" \[\033[0;33m\]%s${NOCOLOR}\")"
     else
         local GITSTR=""
     fi
+
+
 
     #
     # Prompt construction
@@ -67,7 +83,7 @@ function matthew_prompt
 
     # Create the general prompt, and set the command to choose between a
     # good (return = 0) and bad (return != nonzero) version.
-    PROMPT_CONTENT="${TITLESTR}${TIMESTR}${WARNSTR} ${HISTSTR} [${USERSTR}@${HOSTSTR} ${DIRSTR}${GITSTR}]"
+    PROMPT_CONTENT="${TITLESTR}${TIMESTR} ${HISTSTR}${WARNSTR} [${USERSTR}@${HOSTSTR} ${DIRSTR}${GITSTR}]"
     PROMPT_GOOD="${PROMPT_CONTENT}\$ "
     PROMPT_BAD="${PROMPT_CONTENT}\[\033[0;31m\]\$${NOCOLOR} "
     export PROMPT_COMMAND='[ $? = 0 ] && PS1=$PROMPT_GOOD || PS1=$PROMPT_BAD'
