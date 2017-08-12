669db9a7789c30cf220101a16096155a0d749209
Matthew Woitaszek <matthew.woitaszek@gmail.com>
2017-08-12 13:58:25 -0700

    Update ack options

--- bashrc/global_alias.sh
+++ bashrc/global_alias.sh
@@ -16,34 +16,37 @@ alias xsteel="xterm -bg SteelBlue4 -fg 'misty rose' "
 
 # alias ls="ls --color=auto"
 
+
 #
 # git options
 #
+
 alias gitlog="git log --pretty=format:'%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s' --date=short"
 alias gitgraph="git log --pretty=format:'%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s' --date=short --graph"
 
+
 #
-# ack-grep options
+# ack options
 #
 
-export ACK_OPTIONS="--flush --color"
+export ACK_OPTIONS="--flush --color" # --passthru needed only for tailing
 
 function djangotail() {
     tail -n -20 -f $@ | \
-    ack-grep --color-match="green" "celery:" | \
-    ack-grep --color-match="blue" " DEBUG.*" | \
-    ack-grep --color-match="yellow" " WARN.*" | \
-    ack-grep --color-match="red" " ERROR.*" | \
-    ack-grep --color-match="white on_magenta" " CRITICAL.*" | \
-    ack-grep --color-match="white on_red" -i "assert.*| except.*" | \
-    ack-grep --color-match="green" "urlhash=\S+"
+    ack --passthru --color-match="green" "celery:" | \
+    ack --passthru --color-match="blue" " DEBUG.*" | \
+    ack --passthru --color-match="yellow" " WARN.*" | \
+    ack --passthru --color-match="red" " ERROR.*" | \
+    ack --passthru --color-match="white on_magenta" " CRITICAL.*" | \
+    ack --passthru --color-match="white on_red" -i "assert.*| except.*" | \
+    ack --passthru --color-match="green" "urlhash=\S+"
 }
 
 function nginxtail() {
     tail -n -20 -f $@ | \
-    ack-grep --color-match "green"  "(GET|POST|HEAD).* HTTP/1.1\" 2.. " | \
-    ack-grep --color-match "blue"   "(GET|POST|HEAD).* HTTP/1.1\" 3.. " | \
-    ack-grep --color-match="yellow" "(GET|POST|HEAD).* HTTP/1.1\" 4.. " | \
-    ack-grep --color-match="red"    "(GET|POST|HEAD).* HTTP/1.1\" 5.. " 
+    ack --passthru --color-match "green"  "(GET|POST|HEAD).* HTTP/1.1\" 2.. " | \
+    ack --passthru --color-match "blue"   "(GET|POST|HEAD).* HTTP/1.1\" 3.. " | \
+    ack --passthru --color-match="yellow" "(GET|POST|HEAD).* HTTP/1.1\" 4.. " | \
+    ack --passthru --color-match="red"    "(GET|POST|HEAD).* HTTP/1.1\" 5.. "
 }
 
