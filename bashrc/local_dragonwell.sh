bed5bff7580dd0496f979a5e886188c8b6f28a02
Matthew Woitaszek <matthew.woitaszek@gmail.com>
2019-08-31 10:10:56 -0700

    Update profile for dragonwell

--- bashrc/local_dragonwell.sh
+++ bashrc/local_dragonwell.sh
@@ -1,14 +1,14 @@
 #
-# Matthew's profile for 'totoro'
-# MacBook Air / High Sierra
-# October 2018
+# Matthew's profile for 'dragonwell`
+# MacBook Pro / Mojave
+# November 2018
 #
 
 #
 # This is 
-#   .bashrc.local -> ~/.matthew/profile/local_notyet.sh
+#   .bashrc.local -> ~/.matthew/bashrc/local_SYSTEM.sh
 # It relies on variables and functions defined by 
-#   .bashrc.global -> ~/.matthew/profile/global.sh
+#   .bashrc.global -> ~/.matthew/bashrc/global.sh
 # which should be called before this script.
 #
 
@@ -16,11 +16,13 @@
 # Prompt Configuration
 # -------------------------------------
 
+# Git prompt completion
 if [ -e /usr/local/git/contrib/completion/git-completion.bash ]
 then
     source /usr/local/git/contrib/completion/git-completion.bash
 fi
 
+# Custom bash prompt
 PROMPT_HOSTCOLOR="\[\033[0;94m\]"
 matthew_prompt
 
@@ -34,12 +36,13 @@ matthew_prompt
 
 # Path overrides for source packages installed in /opt
 
+
 # -------------------------------------
 # Application Configuration
 # -------------------------------------
 
 # Unison
-export UNISONLOCALHOSTNAME=totoro
+export UNISONLOCALHOSTNAME=dragonwell
 
 # Mac: use ls with colors
 export CLICOLOR="True"
@@ -47,11 +50,12 @@ export CLICOLOR="True"
 # Vim on Mac: Don't rely on X11
 #alias vim="vim -X"
 
-# GNU privacy guard... should be set out-of-repository
+# GNU privacy guard should now be set out-of-repository
 #export GNUPGHOME=""
 
+
 # -------------------------------------
-# Everything Else
+# Google Cloud SDK
 # -------------------------------------
 
 # The next line updates PATH for the Google Cloud SDK.
